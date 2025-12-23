0b8f2fba8f0914f29f8acd4a72a6a16896ad4860
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2025-12-24 08:59:03 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.07 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < k; i++) {
+            answer.append(my_string);
+        }
+        return answer.toString();
+    }
+}
