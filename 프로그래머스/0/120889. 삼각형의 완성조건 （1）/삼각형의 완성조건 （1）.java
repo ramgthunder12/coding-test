5068ea6ace2c720b060da7e35a170dddafca92c6
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2025-12-27 10:21:06 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.02 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        if(sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[2] + sides[1] <= sides[0]) {
+            answer = 2;
+        } else {
+            answer = 1;
+        }
+        return answer;
+    }
+}
