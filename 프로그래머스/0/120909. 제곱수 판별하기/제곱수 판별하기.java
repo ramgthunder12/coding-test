87975aa99d1d68027047810dc6c9e768abf3edaa
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-01-10 08:33:03 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,12 +1,18 @@
+import java.lang.Math;
+
 class Solution {
     public int solution(int n) {
         int answer = 2;
-        for(int i = 1; i <= 1000000; i++ ) {
-            if(n == i * i) {
-                answer = 1;
-                break;
-            }
+        int root = (int)Math.sqrt(n);
+        if(n == root * root) {
+            answer = 1;
         }
+        // for(int i = 1; i <= 1000000; i++ ) {
+        //     if(n == i * i) {
+        //         answer = 1;
+        //         break;
+        //     }
+        // }
         return answer;
     }
 }
