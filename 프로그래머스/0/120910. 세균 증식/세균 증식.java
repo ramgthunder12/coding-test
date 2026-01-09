2c417ec1bc4a3c6e8f83f334c1a5473b07a07122
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-01-10 08:46:11 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,7 +1,8 @@
 import java.lang.Math;
 class Solution {
     public int solution(int n, int t) {
-        int answer = (int)Math.pow((double)2, (double)t) * n;
+        // int answer = (int)Math.pow((double)2, (double)t) * n;
+        int answer = n << t;
         return answer;
     }
 }
