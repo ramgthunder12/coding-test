01e0d29ce54b1d4cbce902d8b31ea08054511212
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-02-23 08:22:47 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int len = names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1;
+        String[] answer = new String[len];
+        for(int i = 0; i < names.length; i = i + 5) {
+            answer[i / 5] = names[i];
+        }
+        return answer;
+    }
+}
