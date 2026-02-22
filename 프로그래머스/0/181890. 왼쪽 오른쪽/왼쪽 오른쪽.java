40225b31cadda7a57db989439346f61b682c8e13
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-02-22 15:13:14 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+        for(int i = 0; i < str_list.length; i++) {
+            String str = str_list[i];
+            if("l".equals(str)) {
+                answer = Arrays.copyOfRange(str_list, 0, i);
+                break;
+            } else if("r".equals(str)) {
+                //u r uu
+                answer = Arrays.copyOfRange(str_list, i + 1, str_list.length);
+                break;
+            }
+        }
+        return answer;
+    }
+}
