1b5441cc9490e0d31d10383f7a90f8105eb606d8
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-03-16 08:48:34 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.30 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        int[] winner = attendRank(rank, attendance);
+        
+        answer = 10000 * winner[0] + 100 * winner[1] + winner[2];
+        return answer;
+    }
+    
+    public int[] attendRank(int[] rank, boolean[] attendance) {
+        int count = 0;
+        int rankNumber = 1;
+        int[] winner = new int[3];
+        
+        while(count < 3) {
+            for(int i = 0; i < rank.length; i++) {
+                if(rankNumber == rank[i] ) {
+                    if(attendance[i]) {
+                        winner[count++] = i;
+                    }
+                }
+            }
+            rankNumber++;
+        }
+        System.out.println(Arrays.toString(winner));
+        return winner;
+    }
+}
