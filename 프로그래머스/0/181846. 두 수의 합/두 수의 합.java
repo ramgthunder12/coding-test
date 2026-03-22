c1f6a52bde793bd66ae8ebb51dc99cb1841c9b10
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-03-23 08:28:41 +0900

    [level 0] Title: 두 수의 합, Time: 437.42 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger bigA = new BigInteger(a);
+        BigInteger bigB = new BigInteger(b);
+        String answer = bigA.add(bigB).toString();
+        return answer;
+    }
+}
