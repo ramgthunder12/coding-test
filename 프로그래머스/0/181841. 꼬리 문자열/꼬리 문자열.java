81ed45df5439eeb9e095b0a6ca535f790befb3f0
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-03-25 20:26:54 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,14 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(String str : str_list) {
+            if(!str.contains(ex)) {
+                answer.append(str);
+            }
+        }
+        return answer.toString();
+    }
+}
