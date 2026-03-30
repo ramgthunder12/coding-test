89e6ba2d67eda7c603e15d80764048fc3e0758ca
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-03-31 08:52:52 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.17 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,17 +1,23 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
+        int answer = 0;
         
-        // year 비교
-        if (date1[0] < date2[0]) return 1;
-        if (date1[0] > date2[0]) return 0;
-
-        // month 비교
-        if (date1[1] < date2[1]) return 1;
-        if (date1[1] > date2[1]) return 0;
-
-        // day 비교
-        if (date1[2] < date2[2]) return 1;
-
-        return 0;
-    }
+        if(date1[0] < date2[0]) {
+            answer = 1;
+        } else if(date1[0] > date2[0]) {
+            answer = 0;
+        } else {
+            if(date1[1] < date2[1]) {
+                answer = 1;
+            } else if (date1[1] > date2[1]) {
+                answer = 0;
+            } else {
+                if(date1[2] < date2[2]) {
+                    answer = 1;
+                    System.out.println("date3 1");
+                }
+            }
+        }
+        return answer;
+    } 
 }
