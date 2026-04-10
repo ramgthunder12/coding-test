0edd680d14ebe2e6219af849a8ae4307d6beeedd
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-10 23:57:35 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.12 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -2,35 +2,25 @@ class Solution {
     public int[][] solution(int n) {
         int[][] arr = new int[n][n];
         
-        int num = 1;
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         
-        while (num <= n * n) {
+        int x = 0, y = 0, dir = 0;
+        
+        for (int i = 1; i <= n * n; i++) {
+            arr[x][y] = i;
             
-            // 1. → 오른쪽
-            for (int i = left; i <= right; i++) {
-                arr[top][i] = num++;
-            }
-            top++;
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
             
-            // 2. ↓ 아래
-            for (int i = top; i <= bottom; i++) {
-                arr[i][right] = num++;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[nx][ny] != 0) {
+                dir = (dir + 1) % 4;
+                nx = x + dx[dir];
+                ny = y + dy[dir];
             }
-            right--;
             
-            // 3. ← 왼쪽
-            for (int i = right; i >= left; i--) {
-                arr[bottom][i] = num++;
-            }
-            bottom--;
-            
-            // 4. ↑ 위
-            for (int i = bottom; i >= top; i--) {
-                arr[i][left] = num++;
-            }
-            left++;
+            x = nx;
+            y = ny;
         }
         
         return arr;
