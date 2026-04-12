7f8e551ccc08b866d50de0660541b074af17a7a7
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-12 21:29:04 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -5,7 +5,9 @@ class Solution {
         int[] dx = {0, 1, 0, -1};
         int[] dy = {1, 0, -1, 0};
         
-        int x = 0, y = 0, dir = 0;
+        int x = 0;
+        int y = 0;
+        int dir = 0;
         
         for (int i = 1; i <= n * n; i++) {
             arr[x][y] = i;
