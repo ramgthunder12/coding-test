d50ca644448425539725ae6c0f6154354f86063d
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-18 22:36:25 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,8 +2,33 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        int[] answer = Arrays.copyOfRange(num_list, 5, num_list.length);
+//         Arrays.sort(num_list);
+//         int[] answer = Arrays.copyOfRange(num_list, 5, num_list.length);
+        
+        int n = num_list.length;
+
+        // 선택 정렬
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (num_list[j] < num_list[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            // swap
+            int temp = num_list[i];
+            num_list[i] = num_list[minIdx];
+            num_list[minIdx] = temp;
+        }
+
+        // 앞 5개 제외하고 결과 생성
+        int[] answer = new int[n - 5];
+        for (int i = 5; i < n; i++) {
+            answer[i - 5] = num_list[i];
+        }
+
         
         return answer;
     }
