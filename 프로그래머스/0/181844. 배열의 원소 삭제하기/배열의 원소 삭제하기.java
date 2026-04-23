5f901df88b9b20554085c6cd261315a8f90f273e
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-23 23:00:52 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.25 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,27 +1,60 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+//     public int[] solution(int[] arr, int[] delete_list) {
+//         ArrayList<Integer> answerList = new ArrayList<>();
+//         HashSet<Integer> set = new HashSet<>();
+        
+//         for(int delete_num : delete_list) {
+//             set.add(delete_num);
+//         }
+        
+//         for(int num : arr) {
+//             if(set.contains(num)) {
+//             } else {
+//                 answerList.add(num);
+//             }
+//         }
+        
+//         int[] answer = new int[answerList.size()];
+//         for(int i = 0; i < answerList.size(); i++) {
+//             answer[i] = answerList.get(i);
+//         }
+
+//         return answer;
+//     }
     public int[] solution(int[] arr, int[] delete_list) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int delete_num : delete_list) {
-            set.add(delete_num);
+        Set<Integer> deleteSet = toSet(delete_list);
+        List<Integer> filtered = filter(arr, deleteSet);
+        return toArray(filtered);
+    }
+
+    private Set<Integer> toSet(int[] deleteList) {
+        Set<Integer> set = new HashSet<>();
+        for (int d : deleteList) {
+            set.add(d);
         }
-        
-        for(int num : arr) {
-            if(set.contains(num)) {
-            } else {
-                answerList.add(num);
+        return set;
+    }
+
+    private List<Integer> filter(int[] arr, Set<Integer> deleteSet) {
+        List<Integer> list = new ArrayList<>();
+        for (int a : arr) {
+            if (!deleteSet.contains(a)) {
+                list.add(a);
             }
         }
-        
-        int[] answer = new int[answerList.size()];
-        for(int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
+        return list;
+    }
 
+    private int[] toArray(List<Integer> list) {
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
