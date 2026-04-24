8e4fd260e9501662f0f91b35e8fc16afe674819a
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-24 18:20:54 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.71 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,24 +1,45 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int[] solution(int[] arr, int k) {
-        int[] answer = new int[k];
-        Arrays.fill(answer, -1);
+//         int[] answer = new int[k];
+//         Arrays.fill(answer, -1);
         
-        HashSet<Integer> set = new HashSet<>();
+//         HashSet<Integer> set = new HashSet<>();
+//         int idx = 0;
+        
+//         for(int i = 0; i < arr.length; i++) {
+//             if(!set.contains(arr[i])) {
+//                 set.add(arr[i]);
+//                 answer[idx++] = arr[i];
+                
+//                 if(idx == k) {
+//                     break;
+//                 }
+//             }
+//         }
+        
+        Set<Integer> set = new HashSet<>();
+        int[] answer = new int[k];
+        
         int idx = 0;
         
-        for(int i = 0; i < arr.length; i++) {
-            if(!set.contains(arr[i])) {
-                set.add(arr[i]);
-                answer[idx++] = arr[i];
+        for (int a : arr) {
+            if (!set.contains(a)) {
+                set.add(a);
+                answer[idx++] = a;
                 
-                if(idx == k) {
-                    break;
-                }
+                if (idx == k) break; // k개 채우면 끝
             }
         }
+        
+        // 남은 자리 -1 채우기
+        while (idx < k) {
+            answer[idx++] = -1;
+        }
+        
         return answer;
     }
 }
