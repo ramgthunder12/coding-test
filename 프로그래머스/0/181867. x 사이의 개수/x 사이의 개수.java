9431f991d416a89652576b7d3b1fd2bf3a678f65
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-04-27 23:07:50 +0900

    Update x 사이의 개수.java

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -2,22 +2,31 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(String myString) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-        int count = 0;
         
-        for(char c : myString.toCharArray()) {
-            if(c == 'x') {
-                answerList.add(count);
-                count = 0;
-            } else {
-                count++;
-            }
+//         ArrayList<Integer> answerList = new ArrayList<>();
+//         int count = 0;
+        
+//         for(char c : myString.toCharArray()) {
+//             if(c == 'x') {
+//                 answerList.add(count);
+//                 count = 0;
+//             } else {
+//                 count++;
+//             }
+//         }
+//         answerList.add(count);
+//         int[] answer = new int[answerList.size()];
+//         for(int i = 0; i < answerList.size(); i++) {
+//             answer[i] = answerList.get(i);
+//         }
+          
+        String[] parts = myString.split("x", -1); // 중요: -1
+        
+        int[] answer = new int[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            answer[i] = parts[i].length();
         }
-        answerList.add(count);
-        int[] answer = new int[answerList.size()];
-        for(int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
+        
         return answer;
     }
 }
