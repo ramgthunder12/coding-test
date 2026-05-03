31caed2797d304fe1bac629d7387194cafef939e
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-05-03 22:11:16 +0900

    Refactor solution method by uncommenting code
    Uncommented code for array initialization and copying.

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[][] solution(int[][] arr) {
-//         int row = arr.length;
-//         int col = arr[0].length;
+        int row = arr.length;
+        int col = arr[0].length;
         
-//         int size = Math.max(row, col);
-//         int[][] answer = new int[size][size];
+        int size = Math.max(row, col);
+        int[][] answer = new int[size][size];
         
-//         for (int i = 0; i < row; i++) {
-//             for (int j = 0; j < col; j++) {
-//                 answer[i][j] = arr[i][j];
-//             }
-//         }
-        int len = Math.max(arr.length, arr[0].length);
-        int[][] answer = new int[len][len];
-        
-        for(int i = 0; i < arr.length; i++) {
-            System.arraycopy(arr[i], 0, answer[i], 0, arr[i].length);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                answer[i][j] = arr[i][j];
+            }
         }
+        
+        // int len = Math.max(arr.length, arr[0].length);
+        // int[][] answer = new int[len][len];
+        
+        // for(int i = 0; i < arr.length; i++) {
+        //     System.arraycopy(arr[i], 0, answer[i], 0, arr[i].length);
+        // }
         
         return answer;
     }
