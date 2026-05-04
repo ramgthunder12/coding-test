b22fde4b8e5f9956bf972667bbbb56a471879911
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2026-05-04 23:45:37 +0900

    Refactor solution method for picture scaling
    Refactored the solution method to improve readability and maintainability. Removed commented-out code and adjusted the logic for repeating rows.

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,40 +1,40 @@
 import java.lang.StringBuilder;
 
 class Solution {
+    // public String[] solution(String[] picture, int k) {
+    //     String[] answer = new String[picture.length * k];
+    //     int idx = 0;
+
+    //     for (String row : picture) {
+    //         StringBuilder sb = new StringBuilder();
+
+    //         // 가로 k배
+    //         for (char c : row.toCharArray()) {
+    //             sb.append(String.valueOf(c).repeat(k));
+    //         }
+
+    //         String newRow = sb.toString();
+
+    //         // 세로 k배
+    //         for (int i = 0; i < k; i++) {
+    //             answer[idx++] = newRow;
+    //         }
+    //     }
+
+    //     return answer;
+    // }
+
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
-        int idx = 0;
-
-        for (String row : picture) {
-            StringBuilder sb = new StringBuilder();
-
-            // 가로 k배
-            for (char c : row.toCharArray()) {
-                sb.append(String.valueOf(c).repeat(k));
-            }
-
-            String newRow = sb.toString();
-
-            // 세로 k배
-            for (int i = 0; i < k; i++) {
-                answer[idx++] = newRow;
+        int count = 0;
+        
+        for(int j = 0; j < picture.length; j++) {
+            for(int i = 0; i < k; i++) {
+                answer[count++] = repeat(picture[j], k);
             }
         }
-
         return answer;
     }
-
-//     public String[] solution(String[] picture, int k) {
-//         String[] answer = new String[picture.length * k];
-//         int count = 0;
-        
-//         for(int j = 0; j < picture.length; j++) {
-//             for(int i = 0; i < k; i++) {
-//                 answer[count++] = repeat(picture[j], k);
-//             }
-//         }
-//         return answer;
-//     }
     
    public String repeat(String str, int k) {
        StringBuilder sb = new StringBuilder();
